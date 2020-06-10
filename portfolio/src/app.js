925c14b31dbfbe2b957c4d6964979567bfeffa23
Michael Sault <msaul025@uottawa.ca>
2020-06-09 23:50:07 -0400

    started learning react
    https://www.udemy.com/share/101v3eAkUedltVRHQ=/

---
+++ portfolio/src/app.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello!</h1>
+                <p>My name is Michael Sault.</p>
+                <p>I am a recent graduate from the Univeristy of Ottawa's computer science program.</p>
+                <p>I am currently looking to expand upon my skills and enter the workforce.</p>
+                <p>My portfolio can be found below.</p>
+            </div>
+        )
+    }
+}
+
+export default App;
+
---
+++ portfolio/src/index.js
@@ -0,0 +1,6 @@
+//libraries needed for react
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './app.js';
+
+ReactDOM.render(<App />, document.getElementById('root'));
