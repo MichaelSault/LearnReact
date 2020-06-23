a9f84ced14b69b7a80ebc51dc93e0450b1c58e01
Michael Sault <michael_sault@hotmail.com>
2020-06-23 00:12:05 -0400

    changed file structure

--- portfolio/src/index.js
+++
@@ -1,9 +0,0 @@
-//note to self --> cd to this dir --> use npm run start to view webpage in browser
-
-//libraries needed for react
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/app.js';
-import './index.css'; //import stylesheet into js page
-
-ReactDOM.render(<App />, document.getElementById('root'));
